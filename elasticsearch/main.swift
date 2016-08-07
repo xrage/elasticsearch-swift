2078b9e86318ba3643b2390111f1341e9d479a3f
xrage <dkverma.vit@gmail.com>
2016-08-07 14:18:46 +0530

    Initial Commit

---
+++ elasticsearch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  elasticsearch
+//
+//  Created by Dharmendra Verma on 07/08/16.
+//  Copyright © 2016 Dharmendra Verma. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
